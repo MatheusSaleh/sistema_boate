4bc25fb0ac1ffb4588728ce08dd49b27adbac77b
MatheusSaleh <matheusxmg2@gmail.com>
2023-06-02 21:04:16 -0300

    estilizado pagina de login

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styles/login.css">
     <title>Pagina de Login</title>
 </head>
 
@@ -52,7 +53,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
     <div class="text-center">
         <h1 class="text-white">Login</h1>
     </div>
-    <div class="container-fluid w-50 bg-white p-4 rounded">
+    <div class="container-fluid w-50 p-4 rounded bg-white bg-opacity-75">
         <form action="" method="POST">
             <p>
                 <label>
